9ff69657717e436aea359c17d97ed50302168840
ShinEunKyu <dmrb2580@gmail.com>
2021-11-09 12:23:50 +0900

    BTL_GUI 3

--- BTL_GUI/BTL_GUI.cpp
+++ BTL_GUI/BTL_GUI.cpp
@@ -91,7 +91,7 @@ void BTL_GUI::on_mining_clicked()
 {
 	QString mining_count = ui.miningcount->text();
 	QString cmd = NODE "-generate " + mining_count + " 2>&1";
-	
+
 	QString result = CmdExe(cmd);
 
 	result = parsing_data(result, "blocks");
@@ -121,7 +121,7 @@ void BTL_GUI::on_mining_clicked()
 void BTL_GUI::on_send_clicked()
 {
 	QString address, amount, fee_rate;
-	address = ui.addresslabel->text();
+	address = ui.sendaddr->text();
 	amount = ui.amount->text();
 	fee_rate = ui.fee_rate->text();
 
@@ -134,24 +134,124 @@ void BTL_GUI::on_send_clicked()
 // 거래 내역 리셋
 void BTL_GUI::on_resettx_clicked()
 {
-	QString cmd = NODE "listtransactions \"*\" 10000";
-	QString result = CmdExe(cmd);
+	/*
+	QString address; // 송수신자의 계좌 주소
+	int amount; // 금액
+	int send_receive; // 송신(1), 수신(2)
+	QString time; // 시간
+	*/
+	TX_info transaction[TX_LIST_COUNT];
+	TX_info prev_tx[TX_LIST_COUNT];
 
-	QJsonDocument doc = QJsonDocument::fromJson(result.toUtf8());
-	QJsonObject JON = doc.object();
+	init_tx_list(prev_tx);
 
-	//QJsonValue value2 = JON.value("");
-	//QJsonArray array2 = value2.toArray();
-
-	//foreach(const QJsonValue & v, array2)
-	//	qDebug() << v.toObject().value("ID").toInt();
-
-	//QJsonArray jsonArray = JON[" "].toArray();
-	//int a = jsonArray.size();
-
-	//QString category = JON.value("time").toString();
+	int compare_count = 0;
+	int tx_start_p = 0;
+	int tx_list = 0;
+	int tx_count = 0;
 
 
+	QString cmd;
+	QString result;
+	QJsonDocument doc;
+	QJsonObject tx_obj;
+	QJsonArray tx_array;
+	QJsonArray tx_array2;
+
+	// 수신 일때, 송신자의 주소를 알기 위한 vin의 정보
+	QJsonObject vin_obj;
+	QJsonArray vin_array;
+	int vin_n;
+	QString vin_txid;
+
+	bool iscontinue = true;
+	while (iscontinue)
+	{
+		cmd = NODE "listtransactions \"*\" " + QString::number(TX_SCAN_COUNT) + " " + QString::number(tx_start_p);
+		result = CmdExe(cmd);
+		if (result == "[\r\n]\r\n")
+			break;
+		doc = QJsonDocument::fromJson(result.toUtf8());
+		tx_array = doc.array();
+		tx_count = tx_array.size();
+
+		for (int i = tx_count - 1; i >= 0; i--)
+		{
+			tx_obj = tx_array[i].toObject();
+			transaction[tx_list].send_receive = tx_obj.value("category").toString();
+			if (transaction[tx_list].send_receive == "send" || transaction[tx_list].send_receive == "receive")
+			{
+				transaction[tx_list].txid = tx_obj.value("txid").toString();
+				if (transaction[tx_list].send_receive == "send") // 송신
+				{
+					transaction[tx_list].address = tx_address_parsing(transaction[tx_list].txid, 0);
+				}
+				else // 수신
+				{
+					cmd = NODE "getrawtransaction " + transaction[tx_list].txid + " true";
+					result = CmdExe(cmd);
+					doc = QJsonDocument::fromJson(result.toUtf8());
+					vin_obj = doc.object();
+
+					vin_array = vin_obj["vin"].toArray();
+					vin_obj = vin_array[0].toObject();
+					vin_n = vin_obj["vout"].toInt();
+					vin_txid = vin_obj["txid"].toString();
+
+					transaction[tx_list].address = tx_address_parsing(vin_txid, vin_n);
+				}
+				transaction[tx_list].amount = tx_obj.value("amount").toDouble();
+				// transaction[tx_list].amount = tx_obj.value("amount").toDouble();
+				if (tx_obj.value("confirmations").toInt() == 0)
+					transaction[tx_list].check_mining = "Mining Fail!";
+				else
+					transaction[tx_list].check_mining = "Mining Success!";
+
+				// 이전 최근 목록과 같으면.. 이후 TX copy
+				if (transaction[tx_list].txid == prev_tx[compare_count].txid && transaction[tx_list].amount == prev_tx[compare_count].amount)
+				{
+					if (transaction[tx_list].check_mining != prev_tx[compare_count].check_mining) compare_count++;
+					else
+					{
+						for (int j = tx_list; j < tx_list + prev_tx[0].save_tx; j++)
+						{
+							transaction[j].txid = prev_tx[j - tx_list].txid;
+							transaction[j].address = prev_tx[j - tx_list].address;
+							transaction[j].amount = prev_tx[j - tx_list].amount;
+							transaction[j].send_receive = prev_tx[j - tx_list].send_receive;
+							transaction[j].check_mining = prev_tx[j - tx_list].check_mining;
+
+							if (j == TX_LIST_COUNT - 1) break;
+						}
+						tx_list++;
+						iscontinue = false;
+						break;
+					}
+				}
+				int time_int = tx_obj.value("time").toInt();
+				time_t epch = time_int + 32400;
+				char *current_time = asctime(gmtime(&epch));
+				transaction[tx_list].time = current_time;
+
+
+				//if (tx_list == 0) transaction[0].tx_obj = tx_obj;
+				tx_list++;
+			}
+			if (tx_list == TX_LIST_COUNT)
+			{
+				iscontinue = false;
+				break;
+			}
+		}
+		tx_start_p += TX_SCAN_COUNT;
+	}
+
+	view_tx_list(transaction);
+
+	// prev_tx[0].tx_obj = tx_array[0].toObject();
+	// memcpy(prev_tx, transaction, sizeof(transaction));
+	// prev_tx[0].tx_obj = transaction[0].tx_obj;
+	prev_tx[0].save_tx = tx_list;
 }
 
 // 대시보드 리셋
@@ -161,11 +261,11 @@ void BTL_GUI::on_resetinfo_clicked()
 	QString getblockcount = CmdExe(cmd);
 	cmd = NODE "getconnectioncount"; // Node 수
 	QString getconnectioncount = CmdExe(cmd);
-	
+
 	int blockcount = getblockcount.toInt();
 
 	QString blockhash[10];
-	
+
 	for (int i = blockcount; i > blockcount - 10; i--)
 	{
 		cmd = NODE "getblockhash " + QString::number(i);
@@ -178,11 +278,11 @@ void BTL_GUI::on_resetinfo_clicked()
 	ui.block7->setText(blockhash[6]); ui.block8->setText(blockhash[7]);
 	ui.block9->setText(blockhash[8]); ui.block10->setText(blockhash[9]);
 
-	ui.blocknum1->setText(QString::number(blockcount)); ui.blocknum2->setText(QString::number(blockcount-1));
-	ui.blocknum3->setText(QString::number(blockcount-2)); ui.blocknum4->setText(QString::number(blockcount-3));
-	ui.blocknum5->setText(QString::number(blockcount-4)); ui.blocknum6->setText(QString::number(blockcount-5));
-	ui.blocknum7->setText(QString::number(blockcount-6)); ui.blocknum8->setText(QString::number(blockcount-7));
-	ui.blocknum9->setText(QString::number(blockcount-8)); ui.blocknum10->setText(QString::number(blockcount-9));
+	ui.blocknum1->setText(QString::number(blockcount)); ui.blocknum2->setText(QString::number(blockcount - 1));
+	ui.blocknum3->setText(QString::number(blockcount - 2)); ui.blocknum4->setText(QString::number(blockcount - 3));
+	ui.blocknum5->setText(QString::number(blockcount - 4)); ui.blocknum6->setText(QString::number(blockcount - 5));
+	ui.blocknum7->setText(QString::number(blockcount - 6)); ui.blocknum8->setText(QString::number(blockcount - 7));
+	ui.blocknum9->setText(QString::number(blockcount - 8)); ui.blocknum10->setText(QString::number(blockcount - 9));
 
 	ui.blockcount->setText(getblockcount);
 	ui.usercount->setText(getconnectioncount);
--- BTL_GUI/BTL_GUI.h
+++ BTL_GUI/BTL_GUI.h
@@ -17,6 +17,20 @@
 #define DATA_DIR "../data"
 #define NODE "bitcoin-cli -pqcnet -datadir=../data -rpcport=1234 "
 
+#define TX_LIST_COUNT 7 // 거래 내역 갱신 갯수 -> 변경 시 출력을 위해 init_tx_list(TX_Info *tx)와 view_tx_list(TX_Info *tx) 함수 수정 필요
+#define TX_SCAN_COUNT 100 // 최근 TX_SCAN_COUNT개의 거래 내역을 조회( TX_SCAN_COUNT번을 주기로 TX_LIST_COUNT만큼 검색 될 때까지 반복 )
+
+typedef struct TX_Info {
+	int save_tx;
+
+	QJsonObject tx_obj;
+	QString txid;
+	QString address; // 송수신자의 계좌 주소
+	double amount; // 금액
+	QString send_receive; // "send" "receive"
+	QString time; // 시간
+	QString check_mining;
+}TX_info;
 
 class BTL_GUI : public QMainWindow
 {
@@ -26,10 +40,6 @@ public:
 	BTL_GUI(QWidget *parent = Q_NULLPTR);
 	QString BTL_GUI::CmdExe(QString cmd);
 	QString wallet_name;
-
-	QList<QString> address; // 상대방의 계좌 주소
-	QList<int> amount; // 송금 금액
-	QList<int> senceive; // 송신 : 1, 수신 : 2
 
 	void start_demon()
 	{
@@ -117,7 +127,7 @@ public:
 		QString version = QString::number(JON.value("version").toInt());
 		QString versionHex = JON.value("versionHex").toString();
 		QString merkleroot = JON.value("merkleroot").toString();
-		QString time = QString::number(JON.value("time").toInt()); // 시간 기준 체크
+		int time_int = JON.value("time").toInt();
 		QString versions = QString::number(JON.value("version").toInt());
 		QString mediantime = QString::number(JON.value("mediantime").toInt());
 		QString nonce = QString::number(JON.value("nonce").toInt());
@@ -130,13 +140,19 @@ public:
 		QString size = QString::number(JON.value("size").toInt());
 		QString weight = QString::number(JON.value("weight").toInt());
 
-		QJsonArray tx_array = JON["txids"].toArray();
+		QJsonArray tx_array = JON["tx"].toArray();
+		// QJsonObject tx_obj = tx_array[0].toObject();
 		QString tx;
 		int tx_count = tx_array.size();
 
+		time_t epch = time_int + 32400;
+		char *current_time = asctime(gmtime(&epch));
+		
+		QString time = current_time;
+
 		// 출력
 		QString view = "hash : " + hash + "\nconfirmations : " + confirmations + "\nheight : " + height + "\nversion : " + versions + \
-			"\nversionHex : " + versionHex + "\nmerkleroot : " + merkleroot + "\ntime : " + time + "\nversion : " + version + \
+			"\nversionHex : " + versionHex + "\nmerkleroot : " + merkleroot + "\ntime : " + time + "version : " + version + \
 			"\nmediantime : " + mediantime + "\nnonce : " + nonce + "\nbits : " + bits + "\ndifficulty : " + difficulty + \
 			"\nchainwork : " + chainwork + "\nnTx : " + nTx + "\npreviousblockhash : " + previousblockhash + "\nstrippedsize : " + strippedsize + \
 			"\nsize : " + size + "\nweight : " + weight + "\nTX : \n";
@@ -145,6 +161,89 @@ public:
 			view = view + tx_array[i].toString() + "\n";
 
 		ui.blockinfo->setText(view);
+	}
+
+	QString tx_address_parsing(QString _tx ,int n)
+	{
+		QString cmd = NODE "getrawtransaction " + _tx + " true";
+		QString result = CmdExe(cmd);
+		QJsonDocument doc = QJsonDocument::fromJson(result.toUtf8());
+		QJsonObject tx_obj = doc.object();
+		
+		QJsonArray tx_array = tx_obj["vout"].toArray();
+
+		int a = tx_array.size();
+		
+		tx_obj = tx_array[0].toObject();
+		tx_obj = tx_obj["scriptPubKey"].toObject();
+		QString res = tx_obj["address"].toString();
+		return res;
+	}
+
+	void init_tx_list(TX_Info *tx)
+	{
+		// amount copy
+		tx[0].amount = ui.tx_amount->text().toDouble(); tx[1].amount = ui.tx_amount_2->text().toDouble();
+		tx[2].amount = ui.tx_amount_3->text().toDouble(); tx[3].amount = ui.tx_amount_4->text().toDouble();
+		tx[4].amount = ui.tx_amount_5->text().toDouble(); tx[5].amount = ui.tx_amount_6->text().toDouble();
+		tx[6].amount = ui.tx_amount_7->text().toDouble();
+
+		// address copy
+		tx[0].address = ui.tx_addr->text(); tx[1].address = ui.tx_addr_2->text();
+		tx[2].address = ui.tx_addr_3->text(); tx[3].address = ui.tx_addr_4->text();
+		tx[4].address = ui.tx_addr_5->text(); tx[5].address = ui.tx_addr_6->text();
+		tx[6].address = ui.tx_addr_7->text();
+
+		// mining copy
+		tx[0].check_mining = ui.tx_mining->text(); tx[1].check_mining = ui.tx_mining_2->text();
+		tx[2].check_mining = ui.tx_mining_3->text(); tx[3].check_mining = ui.tx_mining_4->text();
+		tx[4].check_mining = ui.tx_mining_5->text(); tx[5].check_mining = ui.tx_mining_6->text();
+		tx[6].check_mining = ui.tx_mining_7->text();
+
+		// txid copy
+		tx[0].txid = ui.tx_id->text(); tx[1].txid = ui.tx_id_2->text();
+		tx[2].txid = ui.tx_id_3->text(); tx[3].txid = ui.tx_id_4->text();
+		tx[4].txid = ui.tx_id_5->text(); tx[5].txid = ui.tx_id_6->text();
+		tx[6].txid = ui.tx_id_7->text();
+
+		// send/receive copy
+		tx[0].send_receive = ui.tx_send->text(); tx[1].send_receive = ui.tx_send_2->text();
+		tx[2].send_receive = ui.tx_send_3->text(); tx[3].send_receive = ui.tx_send_4->text();
+		tx[4].send_receive = ui.tx_send_5->text(); tx[5].send_receive = ui.tx_send_6->text();
+		tx[6].send_receive = ui.tx_send_7->text();
+	}
+
+	void view_tx_list(TX_Info *tx)
+	{
+		// amount copy
+		ui.tx_amount->setText(QString::number(tx[0].amount)); ui.tx_amount_2->setText(QString::number(tx[1].amount));
+		ui.tx_amount_3->setText(QString::number(tx[2].amount)); ui.tx_amount_4->setText(QString::number(tx[3].amount));
+		ui.tx_amount_5->setText(QString::number(tx[4].amount)); ui.tx_amount_6->setText(QString::number(tx[5].amount));
+		ui.tx_amount_7->setText(QString::number(tx[6].amount));
+
+		// address copy
+		ui.tx_addr->setText(tx[0].address); ui.tx_addr_2->setText(tx[1].address);
+		ui.tx_addr_3->setText(tx[2].address); ui.tx_addr_4->setText(tx[3].address);
+		ui.tx_addr_5->setText(tx[4].address); ui.tx_addr_6->setText(tx[5].address);
+		ui.tx_addr_7->setText(tx[6].address);
+
+		// mining copy
+		ui.tx_mining->setText(tx[0].check_mining); ui.tx_mining_2->setText(tx[1].check_mining);
+		ui.tx_mining_3->setText(tx[2].check_mining); ui.tx_mining_4->setText(tx[3].check_mining);
+		ui.tx_mining_5->setText(tx[4].check_mining); ui.tx_mining_6->setText(tx[5].check_mining);
+		ui.tx_mining_7->setText(tx[6].check_mining);
+
+		// txid copy
+		ui.tx_id->setText(tx[0].txid); ui.tx_id_2->setText(tx[1].txid);
+		ui.tx_id_3->setText(tx[2].txid); ui.tx_id_4->setText(tx[3].txid);
+		ui.tx_id_5->setText(tx[4].txid); ui.tx_id_6->setText(tx[5].txid);
+		ui.tx_id_7->setText(tx[6].txid);
+
+		// send/receive copy
+		ui.tx_send->setText(tx[0].send_receive); ui.tx_send_2->setText(tx[1].send_receive);
+		ui.tx_send_3->setText(tx[2].send_receive); ui.tx_send_4->setText(tx[3].send_receive);
+		ui.tx_send_5->setText(tx[4].send_receive); ui.tx_send_6->setText(tx[5].send_receive);
+		ui.tx_send_7->setText(tx[6].send_receive);
 	}
 
 
